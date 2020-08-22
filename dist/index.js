a26175a66e396ed3a6a4721db7241c80c1d79538
Mark Tse <mark.tse@neverendingqs.com>
2020-08-22 15:52:51 -0400

    feat: log event payload.

--- dist/index.js
+++ dist/index.js
@@ -167,17 +167,12 @@ const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
+
 
 /***/ }),
 
